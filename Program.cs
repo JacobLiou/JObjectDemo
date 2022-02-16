f3ddb0246dfe9e44401bbbb680769bbe2f55a22e
liumenghui <liumenghui@genomics.cn>
2022-02-16 15:50:15 +0800

    老早的东西了 学习redis 经典分布式组件 进行生产环境搭建 语言访问 经典CS模型  后端 三高

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using StackExchange.Redis;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string ss = "sfsdfs";
+// var objct = JObject.Parse(ss);//Erorr
+// Console.WriteLine(objct);
+
+// https://developer.aliyun.com/article/272212  Redis搭建服务 搭建集群 Ops到生产
+//Redis语言的基础访问 到高阶框架搭建 等等
+var redisConn = ConnectionMultiplexer.Connect("127.0.0.1:8553,connectTimeout=2000");
+if(redisConn.IsConnected)
+{
+    Console.WriteLine("Redis Connn good");
+    var db = redisConn.GetDatabase();
+    Console.WriteLine(db.StringSet("One", "Haha"));
+
+    db.StringSet("Two", "好的");
+}
+
+
+
+
